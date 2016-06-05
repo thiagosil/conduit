e7219242c0764f1b6e1f1e0460919d6f12648b4c
Thiago Assis <thiagosil@gmail.com>
2016-06-05 20:44:14 -0300

    Restricting access to pages for logged in/out users

--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -1,9 +1,12 @@
 export default class User {
-  constructor(AppConstants, $http) {
+  constructor(JWT, AppConstants, $http, $state, $q) {
     'ngInject';
 
+    this._JWT = JWT;
     this._AppConstants = AppConstants;
     this._$http = $http;
+    this._$state = $state;
+    this._$q = $q;
 
     //Object storing user properties
     this.current = null;
@@ -20,6 +23,8 @@ export default class User {
       }).then(
         // On success...
         (res) => {
+          // Set the JWT token
+           this._JWT.save(res.data.user.token);
           // Store the user's info for easy lookup
           this.current = res.data.user;
 
@@ -28,4 +33,63 @@ export default class User {
       );
     }
 
+    logout() {
+      this.current = null;
+      this._JWT.destroy();
+      // Do a hard reload of current state to ensure all data is flushed
+      this._$state.go(this._$state.$current, null, { reload: true });
+    }
+
+    verifyAuth(){
+      let deferred = this._$q.defer();
+
+      // Check for JWT token first
+      if(!this._JWT.get()) {
+        deferred.resolve(false);
+        return deferred.promise;
+      }
+
+      // If there's a JWT and user is already Set
+      if(this.current){
+        deferred.resolve(true);
+        // If current user isn't set, get it from the server.
+        // If server doesn't 401, set current user & resolve promise.
+      } else {
+        this._$http({
+          url: this._AppConstants.api + '/user',
+          method: 'GET'
+        }).then(
+          (res) => {
+            this.current = res.data.user;
+            deferred.resolve(true);
+          },
+          // If an error happens, that means the user's token was invalid.
+          (err) => {
+            this._JWT.destroy();
+            deferred.resolve(false);
+          }
+          // Reject automatically handled by auth interceptor
+          // Will boot them to homepage
+        );
+      }
+
+      return deferred.promise;
+    }
+
+    // This method will be used by UI-Router resolves
+  ensureAuthIs(bool) {
+    let deferred = this._$q.defer();
+
+    this.verifyAuth().then((authValid) => {
+      // if it's the opposite, redirect home
+      if (authValid !== bool) {
+        this._$state.go('app.home');
+        deferred.resolve(false);
+      } else {
+        deferred.resolve(true);
+      }
+    })
+
+    return deferred.promise;
+  } 
 }
