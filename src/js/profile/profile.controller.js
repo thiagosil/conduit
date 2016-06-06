e23a6c8ac3a14d5676b5b65460c0c9e86381069d
Thiago Assis <thiagosil@gmail.com>
2016-06-06 17:10:28 -0300

    Creating user profile

--- src/js/profile/profile.controller.js
+++ src/js/profile/profile.controller.js
@@ -1,6 +1,15 @@
 class ProfileCtrl {
-  constructor() {
+  constructor(profile, User) {
     'ngInject';
+
+    // The profile for this page, resolved by UI Router
+    this.profile = profile;
+
+    if(User.current) {
+      this.isUser = (User.current.username === this.profile.username);
+    } else {
+      this.isUser = false;
+    }
 
   }
 }
