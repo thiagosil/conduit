c27889393c083165b492dae109783fe9271d2615
Thiago Assis <thiagosil@gmail.com>
2016-06-10 13:14:41 -0300

    Users should see articles

--- src/js/article/article.controller.js
+++ src/js/article/article.controller.js
@@ -1,8 +1,18 @@
+import marked from 'marked';
+
 class ArticleCtrl {
-  constructor() {
+  constructor(article, $sce, $rootScope) {
     'ngInject';
 
+    this.article = article;
+
+    $rootScope.setPageTitle(this.article.title);
+  
+    this.article.body = $sce.trustAsHtml(marked(this.article.body, { sanitize: true }));
   }
+
+
+
 }
 
 
--- src/js/services/articles.service.js
+++ src/js/services/articles.service.js
@@ -1,19 +1,48 @@
 export default class Articles {
-  constructor(AppConstants, $http) {
+  constructor(AppConstants, $http, $q) {
     'ngInject';
 
     this._AppConstants = AppConstants;
     this._$http = $http;
+    this._$q = $q;
 
   }
 
   save(article){
-    let request = {
-      url:`${this._AppConstants.api}/articles`,
-      method: 'POST',
-      data: { article: article }
-    };
+    let request = {};
+
+    if (article.slug) {
+      request.url = `${this._AppConstants.api}/articles/${article.slug}`;
+      reques.method = 'PUT';
+      delete article.slug;
+    }
+    else {
+      request.url = `${this._AppConstants.api}/articles`;
+      request.method = 'POST';
+    }
+
+    request.data = { article: article }
 
     return this._$http(request).then((res) => res.data.article);
+  }
+
+  // Retrieve a single article
+  get(slug) {
+    let deferred = this.$q.defer();
+
+    if(!slug.replace(" ", "")){
+      deferred.reject("Article slug is empty");
+      return deferred.promise;
+    }
+
+    this._$http({
+      url: this._AppConstants.api + '/articles/' + slug,
+      method: 'GET'
+    }).then(
+      (res) => deferred.resolve(res.data.article),
+      (err) => deferred.reject(err)
+    );
+
+    return deferred.promise;
   }
 }
